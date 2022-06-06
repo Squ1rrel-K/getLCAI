c719d9e37b2868dda379c27c65bceaba8ca5acd9
Jack <squ1rrel98k@gmail.com>
2022-06-06 16:37:07 +0800

    complete getlcai_test

--- test/getLCAI_test.R
+++ test/getLCAI_test.R
@@ -1,19 +1,24 @@
-# configure your own working directory
-setwd("F:/Code/getLCAI/test")
+# !!! 2 steps are needed to test your data
+# !!! please make sure you have the right configuration
 
-install.packages(
-  "./getLCAI_1.0.0.zip",
-  repos = NULL,
-  type = "win.binary"
-)
-library(getLCAI)
+# step 1: configure your own testing directory
+setwd("E:/Coding/getLCAI/test")
 
-# configure relative path to test the code
+# step 2: configure 5 necessary values
+# Your won exp path and pheno path, relatively
 exp_test_path = "./2/exp_GSE165843.txt"
 pheno_test_path = "./2/GSE165843_phe.txt"
+control_type = "Control"
+experimental_type = "experimental"
+data_type = "Array" # data_type can only be 'Array' or 'RNA-seq'
 
-exp_test <- read.table(exp_test_path)
-pheno_test <- read.table(pheno_test_path)
+install.packages("./getLCAI_1.0.0.zip",
+                 repos = NULL,
+                 type = "win.binary")
+library(getLCAI)
+
+exp_test <- read.table(exp_test_path, header = TRUE)
+pheno_test <- read.table(pheno_test_path, header = TRUE)
 
 #data(exp_example)
 #data(pheno_example)
@@ -21,9 +26,9 @@ pheno_test <- read.table(pheno_test_path)
 outlist = getlcai(
   exp = exp_test_path,
   pheno = pheno_test_path,
-  control = "treated",
-  experimental = "Control",
-  type = 'Array',
+  control = control_type,
+  experimental = experimental_type,
+  type = data_type,
   plotPCA = TRUE
 )
 
