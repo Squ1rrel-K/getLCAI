d3db29bab03a694d06a3e748842c07a5d269dbea
Jack <squ1rrel98k@gmail.com>
2022-06-06 16:47:37 +0800

    fix sth

--- test/getLCAI_test.R
+++ test/getLCAI_test.R
@@ -1,11 +1,12 @@
-# !!! 2 steps are needed to test your data
-# !!! please make sure you have the right configuration
+# !!! 2 steps are needed to test your own data.
+# !!! You can also open getLCAI_test_example.R to see built-in results.
+# !!! please make sure you have the right configuration!
 
 # step 1: configure your own testing directory
 setwd("E:/Coding/getLCAI/test")
 
 # step 2: configure 5 necessary values
-# Your won exp path and pheno path, relatively
+# relative exp path and pheno path
 exp_test_path = "./2/exp_GSE165843.txt"
 pheno_test_path = "./2/GSE165843_phe.txt"
 control_type = "Control"
@@ -19,9 +20,6 @@ library(getLCAI)
 
 exp_test <- read.table(exp_test_path, header = TRUE)
 pheno_test <- read.table(pheno_test_path, header = TRUE)
-
-#data(exp_example)
-#data(pheno_example)
 
 outlist = getlcai(
   exp = exp_test_path,
