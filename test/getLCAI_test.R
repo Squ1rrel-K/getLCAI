040b836e30917436bb1e6e7be402b462eab0525d
Squ1rrel-K <squ1rrel98k@gmail.com>
2022-11-02 19:51:02 +0800

    version 1.0.2 - json output

--- test/getLCAI_test.R
+++ test/getLCAI_test.R
@@ -3,7 +3,7 @@
 # !!! please make sure you have the right configuration!
 
 # step 1: configure your own testing directory
-setwd("E:/Coding/getLCAI/test")
+setwd("./")
 
 # step 2: configure 5 necessary values
 # relative exp path and pheno path
@@ -13,9 +13,10 @@ control_type = "shAMPKa"
 experimental_type = "shCTL"
 data_type = "Array" # data_type can only be 'Array' or 'RNA-seq'
 
-install.packages("./getLCAI_1.0.0.zip",
+install.packages("./getLCAI_1.0.1.zip",
                  repos = NULL,
                  type = "win.binary")
+
 library(getLCAI)
 
 exp_test <- read.table(exp_test_path, header = TRUE)
--- test/getLCAI_test_example.R
+++ test/getLCAI_test_example.R
@@ -1,7 +1,7 @@
 # install
-install.packages("./getLCAI_1.0.0.zip", repos = NULL, type = "win.binary")  # Windows
-install.packages("./getLCAI_1.0.0.zip", repos = NULL, type = "source")      # Linux
-
+setwd("E:/Coding/getLCAI/test")
+# install.packages("./getLCAI_1.0.0.zip", repos = NULL, type = "source")      # Linux
+install.packages("./getLCAI_1.0.0.zip", repos = NULL, type = "win.binary") 
 library(getLCAI)
 data(exp_example)
 data(pheno_example)
